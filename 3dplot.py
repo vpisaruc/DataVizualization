7fbd2ae1679bf08ee4f287835a6e62a5f1b33d72
vpisaruc <vpisaruc@gmail.com>
2019-06-25 15:48:17 +0300

    3d scatter plot

--- 3dplot.py
+++ 3dplot.py
@@ -9,7 +9,15 @@ x = [[1, 2, 3, 4, 5]]
 y = [[4, 5, 1, 7, 5]]
 z = [[1, 5, 9, 5, 2]]
 
-ax1.plot_wireframe(np.array(x), np.array(y), np.array(z))
+#ax1.plot_wireframe(np.array(x), np.array(y), np.array(z))
+
+x2 = [[-1, -2, -3, -4, -5]]
+y2 = [[-4, -5, -1, -7, -5]]
+z2 = [[-1, -5, -9, -5, -2]]
+
+ax1.scatter(x, y, z)
+ax1.scatter(x2, y2, z2)
+
 ax1.set_xlabel('x axis')
 ax1.set_ylabel('y axis')
 ax1.set_zlabel('z axis')
