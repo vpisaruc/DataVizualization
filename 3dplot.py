3b0547480953fd0ab323999e74e378a10df00c8c
vpisaruc <vpisaruc@gmail.com>
2019-06-25 16:03:25 +0300

    Final lesson, if you don't find what you need, you can always visit matplotlib.orh

--- 3dplot.py
+++ 3dplot.py
@@ -7,23 +7,9 @@ style.use('fivethirtyeight')
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 
-x = [1, 2, 3, 4, 5]
-y = [4, 5, 1, 7, 5]
-z = np.zeros(5)
+x, y, z = axes3d.get_test_data()
 
-dx = np.ones(5)
-dy = np.ones(5)
-dz = [1, 2, 3, 4, 5]
-
-ax1.bar3d(x, y, z, dx, dy, dz)
-x2 = [[-1, -2, -3, -4, -5]]
-y2 = [[-4, -5, -1, -7, -5]]
-z2 = [[-1, -5, -9, -5, -2]]
-
-ax1.plot_wireframe(np.array(x2), np.array(y2), np.array(z2))
-
-ax1.scatter(x, y, z)
-ax1.scatter(x2, y2, z2)
+ax1.plot_wireframe(x, y, z, rstride=5, cstride=5)
 
 ax1.set_xlabel('x axis')
 ax1.set_ylabel('y axis')
