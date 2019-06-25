1a8cd0ee9a6b54625dd2e443520c6173f023d90a
vpisaruc <vpisaruc@gmail.com>
2019-06-25 15:58:01 +0300

    3d bar chart

--- 3dplot.py
+++ 3dplot.py
@@ -1,19 +1,26 @@
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import style
 
+style.use('fivethirtyeight')
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 
-x = [[1, 2, 3, 4, 5]]
-y = [[4, 5, 1, 7, 5]]
-z = [[1, 5, 9, 5, 2]]
+x = [1, 2, 3, 4, 5]
+y = [4, 5, 1, 7, 5]
+z = np.zeros(5)
 
-#ax1.plot_wireframe(np.array(x), np.array(y), np.array(z))
+dx = np.ones(5)
+dy = np.ones(5)
+dz = [1, 2, 3, 4, 5]
 
+ax1.bar3d(x, y, z, dx, dy, dz)
 x2 = [[-1, -2, -3, -4, -5]]
 y2 = [[-4, -5, -1, -7, -5]]
 z2 = [[-1, -5, -9, -5, -2]]
+
+ax1.plot_wireframe(np.array(x2), np.array(y2), np.array(z2))
 
 ax1.scatter(x, y, z)
 ax1.scatter(x2, y2, z2)
